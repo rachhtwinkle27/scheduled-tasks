3de3244f5c39ff11fb998824746455dcb9878c11
rachhtwinkle27 <rachhtwinkle27@gmail.com>
2026-06-03 22:56:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+birthday_dict = { 
+    (data_row["month"], data_row["day"]): data_row 
+    for (index, data_row) in data_file.iterrows() 
+}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
@@ -28,7 +31,7 @@ if today_tuple in birthdays_dict:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.sendmail(
@@ -36,3 +39,6 @@ if today_tuple in birthdays_dict:
             to_addrs=birthday_person["email"],
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
+    print("Birthday email sent successfully!") 
+else: 
+    print("No birthdays today.")
