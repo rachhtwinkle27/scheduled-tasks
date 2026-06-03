e28cdab74ed5c3acfe92c27b09c9a3632091264f
rachhtwinkle27 <rachhtwinkle27@gmail.com>
2026-06-03 23:21:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ birthday_dict = {
     (data_row["month"], data_row["day"]): data_row 
     for (index, data_row) in data.iterrows() 
 }
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
